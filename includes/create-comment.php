3a671bfa5698224fe810a109be16307b154d47a7
Nikola <nikolapacariz@gmail.com>
2018-06-07 19:12:54 +0200

    dodatne promene Zadatak8

--- includes/create-comment.php
+++ includes/create-comment.php
@@ -5,17 +5,21 @@ include 'db.php';
 
 if(!isset($_POST['submit'])){
     header("Location: ../index.php");
-
 }else{
     $author = $_POST["Author"];
     $body = $_POST["body"];
     $id = $_POST["Post_id"];
-
+    
+    if(empty($author) || empty($body) ){
+        header("Location: ../single-posts.php?error=empty&id={$id}"); 
+       
+    }else{
         $sql = "INSERT INTO comments (Author,Text,Post_id) VALUES ('$author', '$body', '$id')";
         insertOrDelete($sql,$conn);
            
         header("Location: ../single-posts.php?comment=inserted&id={$id}"); 
     }
+}
     
 
    
